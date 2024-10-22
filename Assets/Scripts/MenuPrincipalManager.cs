07ede3628b802f46a55a048330f3d3e3ee28fcc8
credential.helper <samuelvictor900@gmail.com>
2024-10-22 19:42:45 -0300

    merge: ui; fix: main menu close and openning

--- Assets/Scripts/MenuPrincipalManager.cs
+++ Assets/Scripts/MenuPrincipalManager.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEgine.SceneManagement;
+using UnityEngine.SceneManagement;
+
 public class MenuPrincipalManager : MonoBehaviour
 {
      [SerializeField] private string SelecaoPersonagem;
