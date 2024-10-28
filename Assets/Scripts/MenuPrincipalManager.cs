a2ec334d26c5732c29056cc6def1e4340a3f0de2
Samuel Martins <samuelvictor900@gmail.com>
2024-10-28 18:00:25 -0300

    telas funcinando

--- Assets/Scripts/MenuPrincipalManager.cs
+++ Assets/Scripts/MenuPrincipalManager.cs
@@ -5,22 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipalManager : MonoBehaviour
 {
-     [SerializeField] private string SelecaoPersonagem;
-     [SerializeField] private GameObject painelMenuInicial;
-     [SerializeField] private GameObject painelOpcoes;
-    public void Jogar(){
-        SceneManager.LoadScene(SelecaoPersonagem);
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
 
 
+    void Start()
+    {
+        painelOpcoes.SetActive(false);
     }
-
-    public void AbrirOpcoes(){
+    public void AbrirOpcoes()
+    {
         painelMenuInicial.SetActive(false);
         painelOpcoes.SetActive(true);
 
     }
 
-    public void FecharOpcoes(){
+    public void FecharOpcoes()
+    {
         painelMenuInicial.SetActive(true);
         painelOpcoes.SetActive(false);
 
